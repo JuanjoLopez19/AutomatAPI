3ae41dfcad6a3f2bde9bb2e166d41a2aece5d238
Juanjo López <juanjoselopez@usal.es>
2023-02-15 16:47:48 +0100

    SSL update to flask/express app web

--- templatesStuff/Templates/flask/aux_stuff/app_run/{{cookiecutter.run_folder_name}}/{{cookiecutter.app_name}}.py
+++ templatesStuff/Templates/flask/aux_stuff/app_run/{{cookiecutter.run_folder_name}}/{{cookiecutter.app_name}}.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     {%- endif %}
-    app.run(host='{{cookiecutter.host}}', port='{{cookiecutter.port}}')
+    app.run(host='{{cookiecutter.host}}', port='{{cookiecutter.port}}' {%- if cookiecutter.use_ssl == "yes" %}, ssl_context=('certs/{{cookiecutter.key_name}}.pem', 'certs/{{cookiecutter.cert_name}}.pem'){%- endif %})
