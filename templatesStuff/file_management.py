08835c9784f8f27e1bd1922a7a205fe81016d76d
juanjo <juanjoselopez@usal.es>
2023-03-14 11:42:52 +0100

    Some lint fixes

--- templatesStuff/file_management.py
+++ templatesStuff/file_management.py
@@ -1,8 +1,8 @@
 import shutil, json
 import os
 
-def compress_api(workingDir: str, projectName: str, fileName: str) -> str:
 
+def compress_api(workingDir: str, projectName: str, fileName: str) -> str:
     """
         @param: workingDir: Directory where the project is located
         @param projectName: Name of the project to compress
@@ -13,11 +13,13 @@ def compress_api(workingDir: str, projectName: str, fileName: str) -> str:
 
     DEFAULT_CONFIG = get_default_config()
 
-    output_path = add_base_path(DEFAULT_CONFIG['cookiecutter']['aux_stuff']['output_path'])
+    output_path = add_base_path(
+        DEFAULT_CONFIG['cookiecutter']['aux_stuff']['output_path'])
 
     try:
-        path = shutil.make_archive("{}{}".format(output_path, projectName), 'zip', workingDir) 
-        remove_temp_files(workingDir) 
+        path = shutil.make_archive("{}{}".format(output_path, projectName),
+                                   'zip', workingDir)
+        remove_temp_files(workingDir)
     except (OSError, FileNotFoundError) as e:
         path = None
         print(e)
@@ -25,7 +27,6 @@ def compress_api(workingDir: str, projectName: str, fileName: str) -> str:
 
 
 def remove_temp_files(workingDir: str) -> None:
-
     """
         @param: workingDir: Directory where the temp files are located
         @return: None
@@ -35,23 +36,24 @@ def remove_temp_files(workingDir: str) -> None:
 
 
 def get_default_config() -> dict:
-
     """
         @return: The default configuration for the cookiecutter
     """
-    with open('{}/params.json'.format(os.path.dirname(os.path.abspath(__file__))), 'r') as f:
+    with open(
+            '{}/params.json'.format(os.path.dirname(
+                os.path.abspath(__file__))), 'r') as f:
         config = json.load(f)
     return config
 
-def add_base_path(path: str, *paths) -> str:
 
+def add_base_path(path: str, *paths) -> str:
     """
         @param: path: The path to the file
         @return: The path with the base path
     """
 
     if len(paths) > 0:
-        return os.path.join(os.path.dirname(os.path.abspath(__file__)), path, *paths)
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), path,
+                            *paths)
     else:
         return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    
