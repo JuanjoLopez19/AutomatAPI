6718ac72bfc1e02473f33d3d25ad60cb7cf12e5a
juanjo <juanjoselopez@usal.es>
2023-04-10 21:41:55 +0200

    Django and express services connection

--- nodeBack/routes/templates.routes.ts
+++ nodeBack/routes/templates.routes.ts
@@ -3,7 +3,11 @@ import { Request, Response } from "express";
 import upload from "../middleware/multer.middleware";
 import passport from "passport";
 
-import { makeFlaskTemplate } from "../controllers/templates.controllers";
+import {
+	makeFlaskTemplate,
+	makeExpressTemplate,
+	makeDjangoTemplate,
+} from "../controllers/templates.controllers";
 
 const routerTemplates = Router();
 
@@ -14,6 +18,24 @@ routerTemplates.post(
 		upload.fields([{ name: "cert" }, { name: "key" }]),
 	],
 	makeFlaskTemplate
+);
+
+routerTemplates.post(
+	"/express",
+	[
+		passport.authorize("jwt"),
+		upload.fields([{ name: "cert" }, { name: "key" }]),
+	],
+	makeExpressTemplate
+);
+
+routerTemplates.post(
+	"/django",
+	[
+		passport.authorize("jwt"),
+		upload.fields([{ name: "cert" }, { name: "key" }]),
+	],
+	makeDjangoTemplate
 );
 
 export default routerTemplates;
