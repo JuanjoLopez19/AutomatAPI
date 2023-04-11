7dac4f8c262387acac57d1bc1ee0cd7edf22c0c7
juanjo <juanjoselopez@usal.es>
2023-04-11 09:39:41 +0200

    CloudFront update

--- automatAPI/src/environments/env.prod.dist.ts
+++ automatAPI/src/environments/env.prod.dist.ts
@@ -4,4 +4,7 @@ export const environment = {
   apiPort: '3486',
   googleRoute: '/api/auth/google',
   githubRoute: '/api/auth/github',
+  awsRoute: 'https://d100uzk2jdzk12.cloudfront.net',
+  awsTemplates: '/templates',
+  awsCerts: '/certs',
 };
