1962de5915481b9725a0d297904a855a0848be7f
juanjo <juanjoselopez@usal.es>
2023-04-13 19:15:50 +0200

    Delete user
    Connected to the python back

--- nodeBack/routes/user.routes.ts
+++ nodeBack/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import passport from "../passport";
-import { getUsers } from "../controllers/user.controller";
+import { getUsers, deleteUser } from "../controllers/user.controller";
 import { isAdmin } from "../middleware/auth.middelware";
 
 const routerUser = Router();
 
 routerUser.get("/", [passport.authorize("jwt"), isAdmin], getUsers)
+routerUser.delete("/delete", [passport.authorize("jwt"), isAdmin], deleteUser)
 
 export default routerUser;
