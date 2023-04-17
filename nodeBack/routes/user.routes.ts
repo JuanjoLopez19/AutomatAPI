e0c53785045870a98c093349418f0c0a22b6d8f9
juanjo <juanjoselopez@usal.es>
2023-04-17 12:09:37 +0200

    Account deletion

--- nodeBack/routes/user.routes.ts
+++ nodeBack/routes/user.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import passport from "../passport";
-import { getUsers, deleteUser } from "../controllers/user.controller";
+import { getUsers, deleteUser, deleteAccount } from "../controllers/user.controller";
 import { isAdmin } from "../middleware/auth.middelware";
 
 const routerUser = Router();
 
 routerUser.get("/", [passport.authorize("jwt"), isAdmin], getUsers)
 routerUser.delete("/delete", [passport.authorize("jwt"), isAdmin], deleteUser)
+
+routerUser.delete("/deleteAccount", [passport.authorize("jwt")], deleteAccount)
 
 export default routerUser;
