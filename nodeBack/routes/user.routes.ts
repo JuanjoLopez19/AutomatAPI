f54d62482094c22b67120fbdff4d591dc3ecb92b
juanjo <juanjoselopez@usal.es>
2023-04-19 09:36:09 +0200

    edit account endpoints

--- nodeBack/routes/user.routes.ts
+++ nodeBack/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import passport from "../passport";
-import { getUsers, deleteUser, deleteAccount } from "../controllers/user.controller";
+import { getUsers, deleteUser, deleteAccount, editAccount, editPassword } from "../controllers/user.controller";
 import { isAdmin } from "../middleware/auth.middelware";
 
 const routerUser = Router();
@@ -9,5 +9,8 @@ routerUser.get("/", [passport.authorize("jwt"), isAdmin], getUsers)
 routerUser.delete("/delete", [passport.authorize("jwt"), isAdmin], deleteUser)
 
 routerUser.delete("/deleteAccount", [passport.authorize("jwt")], deleteAccount)
+
+routerUser.put("/editAccount", [passport.authorize("jwt")], editAccount)
+routerUser.put("/editPassword", [passport.authorize("jwt")], editPassword)
 
 export default routerUser;
