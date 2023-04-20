9cb4808e6b2f18e479db217724cb1093a7e7389e
juanjo <juanjoselopez@usal.es>
2023-04-20 11:09:29 +0200

    template edit logic

--- nodeBack/middleware/auxiliaryFunctions.ts
+++ nodeBack/middleware/auxiliaryFunctions.ts
@@ -137,3 +137,22 @@ export const deleteItemsAWS = (certs: string[], template: string) => {
 		else console.log(data); // successful response
 	});
 };
+
+export const deleteItem = (key: string) => {
+AWS.config.update({
+		accessKeyId: config.aws.accessKey,
+		secretAccessKey: config.aws.secretKey,
+		region: config.aws.region,
+	});
+
+	const s3 = new AWS.S3();
+	const params = {
+		Bucket: config.aws.bucket,
+		Key: key,
+	};
+	s3.deleteObject(params, function (err, data) {
+		if (err) console.log(err, err.stack);
+		// an error occurred
+		else console.log(data); // successful response
+	});
+}
